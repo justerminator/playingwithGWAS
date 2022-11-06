d0579f1f40be94865ea2df64c58bc3080cc20406
Justin Lau <justinjlau@yahoo.com>
2022-11-05 22:18:07 -0700

    update

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import openpyxl
 from collections import defaultdict
+from openpyxl.styles import Color, PatternFill, Font, Border
+from openpyxl.styles import colors
+from openpyxl.cell import Cell
+
 
 path = "edited-BSB273CC17HSLGA15_output (1).xlsx"
+
+
 
 # open workbook
 wb_obj = openpyxl.load_workbook(path)
@@ -12,7 +18,7 @@ sheet_obj = wb_obj.active
 # range of animal categories
 row = 4
 column = 18
-burst = 180
+interval = 180
 
 listAnimals = []
 for i in range(2, 18):
@@ -29,26 +35,40 @@ x = 15
 dictDur = defaultdict(list)
 anCounter = 0
 
+numBursts = 2
+
 # animal for each column
 for animal in range(2, xValue):
-    # initialize start time
-    ptr1 = sheet_obj.cell(x, animal).value
 
-    listDur = []
-    listDur.append((sheet_obj.cell(x, 1).value, ptr1))
-    while x != colEnd:
-        if sheet_obj.cell(x, animal).value - ptr1 >= burst:
-            ptr1 = sheet_obj.cell(x, animal).value
-            listDur.append((sheet_obj.cell(x, 1).value, ptr1))
-        x += 1
+    allBursts = []
+    i = 15
+    while i != colEnd:
+        if sheet_obj.cell(i, animal).value == 0:
+            break
+        # initialize start time
+        oneBurst = []
+        ptr1 = sheet_obj.cell(i, animal).value
+        limit = ptr1 + interval
 
-    dictDur[listAnimals[anCounter]] = listDur.copy()
+        j = i
+        while j != colEnd and sheet_obj.cell(j, animal).value <= limit:
+            if sheet_obj.cell(j, animal).value == 0:
+                break
+            oneBurst.append((sheet_obj.cell(j, 1).value, sheet_obj.cell(j, animal).value))
+            j += 1
+        if len(oneBurst) > numBursts:
+            allBursts.append(oneBurst)
+        i = j
+
+
+
+    dictDur[listAnimals[anCounter]] = allBursts.copy()
     anCounter += 1
     x = 15
 
 
 
-print(len(dictDur))
+print(dictDur)
 
 newSheet = openpyxl.Workbook()
 sheet = newSheet.active
@@ -64,15 +84,28 @@ for i in range(len(listAnimals)):
 # populate data
 rowCoord = 2
 colCoord = 2
+
+grayfill = PatternFill(start_color='00808080', end_color='00808080', fill_type='solid')
 for x in range(len(dictDur)):
 
     for y in range(len(dictDur[listAnimals[x]])):
 
-        c2 = sheet.cell(rowCoord, colCoord)
-        c2.value = dictDur[listAnimals[x]][y][0]
+        for z in range(len(dictDur[listAnimals[x]][y])):
 
-        c3 = sheet.cell(rowCoord, colCoord + 1)
-        c3.value = dictDur[listAnimals[x]][y][1]
+            c2 = sheet.cell(rowCoord, colCoord)
+
+            c2.value = dictDur[listAnimals[x]][y][z][0]
+
+            c3 = sheet.cell(rowCoord, colCoord + 1)
+            c3.value = dictDur[listAnimals[x]][y][z][1]
+
+            rowCoord += 1
+
+        c4 = sheet.cell(rowCoord, colCoord)
+        c4.fill = grayfill
+
+        c5 = sheet.cell(rowCoord, colCoord + 1)
+        c5.fill = grayfill
 
         rowCoord += 1
 
@@ -80,4 +113,4 @@ for x in range(len(dictDur)):
     rowCoord = 2
     colCoord += 2
 
-newSheet.save("sample.xlsx")
+newSheet.save("newfile.xlsx")
