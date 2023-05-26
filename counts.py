a796369fca8cc2b8ca8d7009fc9d9eddd67a311b
Justin Lau <81498402+justerminator@users.noreply.github.com>
2023-05-25 22:21:10 -0700

    Update counts.py

--- counts.py
+++ counts.py
@@ -10,19 +10,25 @@ import pandas as pd
 import ast
 import openpyxl
 
-data = pd.read_excel("Full_burst_LGA14_long.xlsx")
+data = pd.read_excel("xlFull_burst_all_long.xlsx")
 
 
-data_oxy = data[data["drug_group"] == 'Oxycodone']
-iir_data_col = data_oxy[["inter_infusion_interval_LGA14", "rat"]]
+data_oxy = data[data["drug_group"] == 'Cocaine']
+iir_data_col = data_oxy[["inter_infusion_interval_LGA03", "rat"]]
 iir_data_col = iir_data_col.dropna()
 
 
 set_intervals = [(20, 39), (40, 59), (60, 79), (80, 99), (100, 119), (120, 139)]
+set_intervals2 = [(20, 29), (30, 49), (50, 59), (60, 69), (70, 79), (80, 89), (90, 99), (100, 109), (110, 119), (120, 139)]
 
 wb = openpyxl.Workbook()
 ws = wb.active
 #sheet = wb['Sheet1']
+
+#TODO: Look at LGA 03, or earlier cohorts
+
+newWb = openpyxl.Workbook()
+newSheet = newWb.active
 
 # for x in range(1, len(set_intervals) + 1):
 #
@@ -31,37 +37,43 @@ ws = wb.active
 
 rowTracker = 2
 
+mapSum = defaultdict(int)
+
+for x in range(len(set_intervals2)):
+    mapSum[str(set_intervals2[x])] = 0
+
 def createCounts(lst, rat):
 
     global rowTracker
 
     map_counts = defaultdict(int)
 
-    for x in range(len(set_intervals)):
-        map_counts[str(set_intervals[x])] = 0
+    for x in range(len(set_intervals2)):
+        map_counts[str(set_intervals2[x])] = 0
 
     intermed = ast.literal_eval(lst)
     for val in intermed:
-        for ind in range(len(set_intervals)):
+        for ind in range(len(set_intervals2)):
 
-            if set_intervals[ind][0] <= val <= set_intervals[ind][1]:
+            if set_intervals2[ind][0] <= val <= set_intervals2[ind][1]:
 
-                map_counts[str(set_intervals[ind])] += 1
+                map_counts[str(set_intervals2[ind])] += 1
+                mapSum[str(set_intervals2[ind])] += 1
                 print(map_counts)
 
 
 
     # writing to file
 
-    for i in range(0, 6):
+    for i in range(0, len(set_intervals2)):
         c2 = ws.cell(rowTracker, 1)
         c2.value = str(rat)
 
         c3 = ws.cell(rowTracker, 2)
-        c3.value = str(set_intervals[i])
+        c3.value = int(i + 1)
 
         c4 = ws.cell(rowTracker, 3)
-        c4.value = str(map_counts[str(set_intervals[i])])
+        c4.value = int(map_counts[str(set_intervals2[i])])
 
         rowTracker += 1
 
@@ -91,10 +103,17 @@ def createCounts(lst, rat):
 for index, row in iir_data_col.iterrows():
 
     leRat = row["rat"]
-    leLst = row["inter_infusion_interval_LGA14"]
+    leLst = row["inter_infusion_interval_LGA03"]
 
 
     map = createCounts(leLst, leRat)
 
+for index in range(0, len(set_intervals2)):
+    c5 = newSheet.cell(index + 1, 1)
+    c5.value = str(set_intervals2[index])
 
-wb.save('newfile.xlsx')
+    c6 = newSheet.cell(index + 1, 2)
+    c6.value = mapSum[str(set_intervals2[index])]
+
+newWb.save("new.xlsx")
+wb.save('newfile2.xlsx')
